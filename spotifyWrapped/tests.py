33ce90a3f977b10e1ed889fd3d2058064a0e8960
IanVa <ianvalderas@outlook.com>
2024-10-18 23:37:35 -0400

    Setting up for adding more points to project through the github testing process

--- spotifyWrapped/tests.py
+++ spotifyWrapped/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import UserProfile
 
-# Create your tests here.
+class UserProfileTest(TestCase):
+    def test_user_profile_creation(self):
+        # Test user profile creation logic
+        pass
